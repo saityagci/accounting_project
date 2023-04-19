3b0c8e5743f1a53f93f6d0b7075c66ea2a0f2f5d
saityagci <saityagci@gmail.com>
2023-04-18 21:19:07 -0400

    accounting

---
+++ src/main/java/com/cydeo/accounting_project/AccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.accounting_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/accounting_project/AccountingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.accounting_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
